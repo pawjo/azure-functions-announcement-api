028c1f14423b1a3aa285c2cbebe0c3247d9a4df0
PawelWiszniewski <61913429+pawjo@users.noreply.github.com>
2023-03-30 23:48:13 +0200

    Update AnnouncementFAContext.cs

--- AnnouncementsApiFunctionsApp/Domain/AnnouncementFAContext.cs
+++ AnnouncementsApiFunctionsApp/Domain/AnnouncementFAContext.cs
@@ -25,8 +25,7 @@ namespace AnnouncementsApiFunctionsApp.Domain
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=BILL-CZ9R9C3;Initial Catalog=AnnouncementFA;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                optionsBuilder.UseSqlServer("<CONNECTION_STRING>");
             }
         }
 
