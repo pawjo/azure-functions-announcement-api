726f8d0c4fc8601dcf0e3b37555f67a5ae6617eb
PawelWiszniewski <61913429+pawjo@users.noreply.github.com>
2023-03-30 23:49:10 +0200

    Removed access modifiers

--- AnnouncementsApiFunctionsApp/Logic/IAnnouncementService.cs
+++ AnnouncementsApiFunctionsApp/Logic/IAnnouncementService.cs
@@ -7,14 +7,14 @@ namespace AnnouncementsApiFunctionsApp
 {
     public interface IAnnouncementService
     {
-        public Task<Result<int>> AddAsync(AddAnnouncementRequest request);
+        Task<Result<int>> AddAsync(AddAnnouncementRequest request);
 
-        public Task<Result<bool>> DeleteAsync(int id);
+        Task<Result<bool>> DeleteAsync(int id);
 
-        public Task<Result<AnnouncementResponse>> GetByIdAsync(int id);
+        Task<Result<AnnouncementResponse>> GetByIdAsync(int id);
 
-        public Task<Result<List<AnnouncementResponse>>> GetListAsync(string searchText, string typeFilter, int pageNumber, int elementsOnPage);
+        Task<Result<List<AnnouncementResponse>>> GetListAsync(string searchText, string typeFilter, int pageNumber, int elementsOnPage);
 
-        public Task<Result<bool>> UpdateAsync(UpdateAnnouncementRequest request);
+        Task<Result<bool>> UpdateAsync(UpdateAnnouncementRequest request);
     }
 }
